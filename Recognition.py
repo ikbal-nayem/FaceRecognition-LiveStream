b999d2995e8d8c6504e5572c4e38918128584207
Ikbal Nayem <ikbalnayem000@gmail.com>
2021-04-04 15:31:41 +0600

    edited

--- Recognition.py
+++ Recognition.py
@@ -1,5 +1,6 @@
 import os
 import joblib
+import time
 from PIL import Image
 from face_recognition import preprocessing
 
@@ -8,13 +9,15 @@ file_loc = os.path.dirname(os.path.abspath(__file__))
 face_recogniser = joblib.load(os.path.join(file_loc, 'model', 'face_recogniser.pkl'))
 preprocess = preprocessing.ExifOrientationNormalize()
 
-
+pT = 0
 def recognize_cv2(img):
+    cT = time.time()
     img = Image.fromarray(img)
+    print(cT-pT)
+    pT = cT
     img = preprocess(img)
     faces = face_recogniser(img)
-    faces_data = [{
-                    'top_prediction': face['top_prediction'],
-                    'bounding_box': face['bb']
-                } for face in faces]
-    return {"faces": faces_data}
+    return [{
+                'top_prediction': face['top_prediction'],
+                'bounding_box': face['bb']
+            } for face in faces]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import time
 from Recognition import recognize_cv2
 
-
+print('Starting webcam...')
 capture = cv2.VideoCapture(0)
 capture.set(3, 720)
 capture.set(4, 480)
@@ -11,14 +11,14 @@ pTime = 0
 while True:
     check, frame = capture.read()
     faces = recognize_cv2(frame)
-    if len(faces['faces']) > 0:
-      for face in faces['faces']:
+    if len(faces) > 0:
+      for face in faces:
         top = int(face['bounding_box']['top'])
         bottom = int(face['bounding_box']['bottom'])
         left = int(face['bounding_box']['left'])
         right = int(face['bounding_box']['right'])
         cv2.rectangle(frame, (left, top), (right, bottom), (100, 255, 0), 1)
-        confidence = int(face['top_prediction']['confidence']*100)
+        confidence = "{:.2f}".format(face['top_prediction']['confidence']*100)
         face_label = str(f"{face['top_prediction']['label']} ({confidence})") if confidence>90 else "?"
         cv2.putText(frame, face_label, (left, top), cv2.LINE_AA, .5, (100,0,200), 2)
     cTime = time.time()
